9242024431a318f3dbc7006e469050562887b933
fonchovega <alfonso.vega.huertas@gmail.com>
2025-11-17 06:12:22 -0600

    Create normalize_historico_v134.js

---
+++ scripts/normalize_historico_v134.js
@@ -0,0 +1,5 @@
+// normalize_historico_v134.js
+// Puente para v1.3.4: mantiene el nombre que usa el workflow
+// y delega el trabajo real a normalizador_historico_v134.js
+
+import "./normalizador_historico_v134.js";
